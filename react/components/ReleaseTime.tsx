5b1f8ece5e4b2a2a78cee8ec8a53f06fc416717b
Leonardo Padilha <lcfpadilha@gmail.com>
2018-08-22 13:38:52 -0300

    Make releases time responsive

--- react/components/ReleaseTime.tsx
+++ react/components/ReleaseTime.tsx
@@ -15,9 +15,9 @@ class ReleaseTime extends Component<ReleaseTimeProps> {
   public renderDate = (date: Moment) => {
     const curDate = moment(Date.now())
     const curYear = curDate.year()
-    
+
     return (
-      <div className="flex flex-column mr8 items-center">
+      <div className="flex flex-column mr8-ns items-center">
         <span className="f2 fw3 rebel-pink">{date.date()}</span>
         <span className="f3 fw5 black">{MONTHS[date.month()]}</span>
         {curYear !== date.year() ? <span className="f3 fw5 light-gray">{date.year()}</span> : null}
@@ -38,13 +38,13 @@ class ReleaseTime extends Component<ReleaseTimeProps> {
     const { canAddDate, releaseDate } = this.props
     return (
       <NoSSR onSSR={this.renderSkeleton()}>
-        <div className="flex flex-row justify-end w-25 pr6">
+        <div className="flex flex-column flex-row-ns justify-center justify-end-ns w-20 w-25-ns pr6">
           {canAddDate ?
             this.renderDate(releaseDate)
             : null
           }
-          <div className="h-100 flex self-end pt8">
-            <span className="f3 fw5 silver">{releaseDate.format('HH:mm')}</span>
+          <div className="h-100-ns flex self-center self-end-ns pt5 pt8-ns">
+            <span className="f5 f3-ns fw5 gray silver-ns">{releaseDate.format('HH:mm')}</span>
           </div>
         </div>
       </NoSSR>
