2764b907c83b002d54c75e0f91842e19f4a182a3
Leonardo Padilha <lcfpadilha@gmail.com>
2018-08-23 00:00:46 -0300

    Add profile name at top of the page

--- react/ReleasesPage.tsx
+++ react/ReleasesPage.tsx
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
 
 import ReleasesContent from './components/ReleasesContent'
+
+import Profile from './queries/Profile.graphql'
+
 import LogoutIcon from './icons/LogoutIcon'
 import VtexIcon from './icons/VtexIcon'
 
 import './releases.global.css'
 
+interface ProfileData {
+  profile: any
+}
 
-class ReleasesPage extends Component {
+class ReleasesPage extends Component<{} & ProfileData> {
   public render() {
+    const { profile: { profile } } = this.props
+
+    const firstName = profile
+      ? profile.name.split(' ')[0]
+      : ''
+
     return (
       <div className="w-100 h-100 bg-light-silver overflow-hidden flex flex-column">
         <div className="w-100 flex flex-row flex-none justify-between pa7">
           <VtexIcon />
           <div className="flex flex-row items-center justify-center">
-            <p>lcfpadilha@gmail.com</p>
+            <p>{firstName}</p>
             <div className="pl3 pointer">
               <LogoutIcon />
             </div>
@@ -24,17 +37,11 @@ class ReleasesPage extends Component {
       </div>
     )
   }
-
-  private handleEnvChange = (event: any) => {
-    const env = event.target.value as Environment
-
-    this.setState((prevState) => {
-      return ({
-        ...prevState,
-        env
-      })
-    })
-  }
 }
 
-export default ReleasesPage
+export default graphql<ProfileData>(Profile, {
+  name: 'profile',
+  options: {
+    ssr: false
+  }
+})(ReleasesPage)
